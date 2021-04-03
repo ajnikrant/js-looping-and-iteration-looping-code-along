23c318ca3e664a31f765af0cd869688c07d8f433
Amanda <ajnikrant@gmail.com>
2021-04-03 18:52:32 -0400

    commit tree 51b41e7bd27ac84864cca552f9d977a76b485dbc

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+cards = ["Ada", "Brennan", "Ali"];
+
+function writeCards (cards){
+    let i=0;
+    while (i<cards.length){
+        console.log(`Thank you ${cards[i]} for the lovely suprise gift!`);
+        i++;
+    }
+}
+
+function countDown(num){
+    while (num > 0){
+        console.log(num-=1);
+
+    }
+    
+}
